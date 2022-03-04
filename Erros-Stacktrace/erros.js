3c893458e00bd595686fce9dfff562fab1ceee21
carlacou <carlacoutinhopontes@gmail.com>
2022-03-04 10:59:11 -0300

    entendo-os-erros

---
+++ Erros-Stacktrace/erros.js
@@ -0,0 +1,20 @@
+// SyntaxError: Missing initializer in const declaration
+//const numero; 
+
+//--------------------------------------------------------
+
+// variávies const devem ser declaradas com valor inicial
+//const number = 0;
+
+//--------------------------------------------------------
+
+// SyntaxError: missing ) after argument list => linha 8
+//console.log(numero1
+// const numero1 = 1;
+// console.log(numero1
+
+  //--------------------------------------------------------
+
+  // ReferenceError: minhaVar is not defined => linha 19:15
+  const num = 1;
+  console.log(minhaVar); // não foi declarada
