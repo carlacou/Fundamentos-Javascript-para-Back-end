042f33daa5d63835fb1a122a3c14fe27d1a1fa3e
carlacou <carlacoutinhopontes@gmail.com>
2022-03-04 11:21:07 -0300

    inclusao-de-outros-métodos-console

--- Console/console.js
+++ Console/console.js
@@ -1,15 +1,24 @@
 // log => registro
-// podemos colocar qualquer informação dentro desse registro, podendo ser um número, uma string, uma variável, o que quisermos. 
+// podemos colocar qualquer informação dentro desse registro, podendo ser um número, uma string, uma variável, o que quisermos.
 
 minhaVar = true;
 
 console.log(245);
-console.log('Sou um texto');
+console.log("Sou um texto");
 console.log(minhaVar);
 
 // tratamento de erro!
-// É sempre uma boa prática nós utilizarmos o console, 
-// tanto quanto log quanto error, para sair da nossa aplicação, 
+// É sempre uma boa prática nós utilizarmos o console,
+// tanto quanto log quanto error, para sair da nossa aplicação,
 // para sempre sabermos o que está acontecendo dentro dela.
 
-console.error('deu erro!')
+console.error("deu erro!");
+
+/*
+  Entre os outros métodos, existem:
+
+console.error() para exibir mensagens de erro;
+console.table() para visualizar de forma mais organizada informações tabulares;
+console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
+*/
