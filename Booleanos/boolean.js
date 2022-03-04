57044fff0b24c7b6ceac4247d4d55bee9220c98f
carlacou <carlacoutinhopontes@gmail.com>
2022-03-03 23:25:50 -0300

    booleanos-verdadeiro-ou-falso

---
+++ Booleanos/boolean.js
@@ -0,0 +1,14 @@
+// boolean => valores true ou false
+
+const usuarioLigado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 => representa false
+// 1 => representa true
+
+console.log(0 == false);
+console.log('' == false)
+console.log(1 == true)
+
