3a0fe864e8a29973086f2b4097194620731dbac7
carlacou <carlacoutinhopontes@gmail.com>
2022-03-03 21:42:22 -0300

    tipos-de-dados

---
+++ Tipos-de-dados/tipo-numero.js
@@ -0,0 +1,40 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+
+console.log(operacaoMatematica);
+/********************************* */
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+/********************************* */
+
+// NaN => Not A Number (não é um número)
+
+const alura = "Alura";
+console.log(alura - primeiroNumero);
+
+
+/********************************* */
+/**Divisão por zero não é um erro em JavaScript: ele simplesmente retorna “Infinity”. No entanto, há uma exceção: zero dividido por zero não tem um valor bem definido e o resultado dessa operação é o valor especial não numérico NaN. */
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
---
+++ Tipos-de-dados/tipo-string.js
@@ -0,0 +1,28 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = "Olá, mundo!"
+const senha = 'senhaSuperSegura456!' // cadeias de caracteres incluindo texto e numero e caracteres especiasis.
+const sringDeNumeros = '34567';
+
+const citacao = 'Leo disse "Oi!"'; // aspas duplas  e simples são usadas juntas em caso de citação.
+console.log(citacao);
+
+// concatenação (+)
+
+const frase = 'Meu nome é ';
+const meuNome = 'Fernanda'; 
+
+console.log(frase + meuNome)
+
+// template string ou template literal
+
+console.log(`${frase}${meuNome}`);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
