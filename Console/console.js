46de84379211bc6d5362fd668073a9f4f2ea4089
carlacou <carlacoutinhopontes@gmail.com>
2022-03-04 11:16:46 -0300

    console-output-node-e-browser

---
+++ Console/console.js
@@ -0,0 +1,15 @@
+// log => registro
+// podemos colocar qualquer informação dentro desse registro, podendo ser um número, uma string, uma variável, o que quisermos. 
+
+minhaVar = true;
+
+console.log(245);
+console.log('Sou um texto');
+console.log(minhaVar);
+
+// tratamento de erro!
+// É sempre uma boa prática nós utilizarmos o console, 
+// tanto quanto log quanto error, para sair da nossa aplicação, 
+// para sempre sabermos o que está acontecendo dentro dela.
+
+console.error('deu erro!')
