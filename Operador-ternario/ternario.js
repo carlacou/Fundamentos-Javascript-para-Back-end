ca4c6e75d9953fd92c002dc0d224b5ee07609d5b
carlacou <carlacoutinhopontes@gmail.com>
2022-03-04 19:28:22 -0300

    operador-ternario

---
+++ Operador-ternario/ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+const idadeCliente2 = 19
+
+// if(idadeCliente >= idadeMinima) {
+//   console.log('cerveja');
+// } else {
+//   console.log('suco');
+// }
+
+             // condição                    // true    // false
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+console.log(idadeCliente2 >= idadeMinima ? "cerveja" : "suco")
